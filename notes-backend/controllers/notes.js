7e16d12d7ef302d37697307fca81fc12e7065a84
Christian Simmons <CSimmonsCS@gmail.com>
2025-06-14 12:55:30 -0700

    adding tests and async/await to project

--- notes-backend/controllers/notes.js
+++ notes-backend/controllers/notes.js
@@ -1,52 +1,35 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
 
-notesRouter.get("/", (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes);
-  });
+notesRouter.get("/", async (request, response) => {
+  const notes = await Note.find({});
+  response.json(notes);
 });
 
-notesRouter.get("/:id", (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
-});
-
-notesRouter.post("/", (request, response, next) => {
-  const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+notesRouter.get("/:id", async (request, response, next) => {
+  const note = await Note.findById(request.params.id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
   }
+});
+
+notesRouter.post("/", async (request, response) => {
+  const body = request.body;
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   });
 
-  note
-    .save()
-    .then((savedNote) => {
-      response.json(savedNote);
-    })
-    .catch((error) => next(error));
+  const saved = await note.save();
+  response.status(201).json(saved);
 });
 
-notesRouter.delete("/:id", (request, response) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then((result) => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
+notesRouter.delete("/:id", async (request, response) => {
+  await Note.findByIdAndDelete(request.params.id);
+  response.status(204).end();
 });
 
 notesRouter.put("/:id", (request, response, next) => {
